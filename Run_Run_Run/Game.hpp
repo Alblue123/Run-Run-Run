41bed6e24d5fbed0a97c4fe36547810976c28c68
Alblue123 <newborniphone@gmail.com>
2024-07-18 15:39:48 +0700

    Initialize program

--- Run_Run_Run/Game.hpp
+++ Run_Run_Run/Game.hpp
@@ -1,1 +1,20 @@
+#pragma once
 
+#include <SDL.h>
+#include <windows.h>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+static SDL_Window* gWindow = NULL;
+static SDL_Renderer* gRenderer = NULL;
+static SDL_Event e;
+
+//Screen
+const int FPS = 30;
+const int SCREEN_WIDTH = 1024;
+const int SCREEN_HEIGHT = 512;
+const int gPixelBit = 32;
+
+void start();
--- Run_Run_Run/main.cpp
+++ Run_Run_Run/main.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+#include <SDL.h>
+#include "Game.hpp"
 
+using namespace std;
+
+int main(int argc, char **argv) {
+    start();
+}
