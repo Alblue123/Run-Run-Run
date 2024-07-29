8dbeb767a161b99c796a3663a5a52387f1871574
Alblue123 <newborniphone@gmail.com>
2024-07-29 11:26:18 +0700

    adding helper function for image

---
+++ Run_Run_Run/Image.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <SDL.h>
+#include "GameWindow.hpp"
+
+class Image:
+{
+public:
+    Image(GameWindow* mWin);
+    ~Image();
+
+    SDL_Rect getRect();
+    void setPosCenter(Vector2 pos);
+    void setPosCenter(int x, int y);
+    void setHeight(int h);
+    void draw();
+    void loadTexture(const char* path);
+    void update();
+    Vector2 getPos();
+    float scale;
+
+private:
+    Vector2 pos;
+    GameWindow* mWin;
+    SDL_Rect rex;
+    SDL_Texture* mTex;
+
+};
