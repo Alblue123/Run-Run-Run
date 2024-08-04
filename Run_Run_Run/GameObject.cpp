f843cb84310bb700f9642ae0831fc949828ac2cb
Alblue123 <newborniphone@gmail.com>
2024-08-04 19:58:58 +0700

    adding class for controlling game object

---
+++ Run_Run_Run/GameObject.cpp
@@ -0,0 +1,68 @@
+#include "GameObject.hpp"
+
+GameObject::GameObject()
+{
+    mTexture = NULL;
+    rect = { 0, 0, 0 , 0 };
+}
+
+GameObject::~GameObject()
+{
+    free();
+}
+
+void GameObject::movePos(std::pair<int, int> pos) {
+    rect.x += pos.first;
+    rect.y += pos.second;
+}
+
+bool GameObject::loadIMG(std::string path, SDL_Renderer* screen)
+{
+    free(); //free pre-existed objects
+
+    SDL_Texture* newTexture = NULL;
+
+    SDL_Surface* loadSurface = IMG_Load(path.c_str());
+    if (loadSurface == NULL)
+    {
+        std::cout << "Unable to load image! SDL_image Error:" << path << ", " << IMG_GetError() << std::endl;
+    }
+    else
+    {
+        SDL_SetColorKey(loadSurface, SDL_TRUE, SDL_MapRGB(loadSurface->format, COLOR_KEY_R, COLOR_KEY_G, COLOR_KEY_B));
+        newTexture = SDL_CreateTextureFromSurface(screen, loadSurface);
+        if (newTexture == NULL)
+        {
+            std::cout << "Unable to create texture from: " << path << ", " << SDL_GetError() << std::endl;
+        }
+        else
+        {
+            rect.w = loadSurface->w;
+            rect.h = loadSurface->h;
+        }
+        SDL_FreeSurface(loadSurface);
+    }
+    mTexture = newTexture;
+
+    return mTexture != NULL;
+}
+
+void GameObject::render(SDL_Renderer* renderer, bool flip, SDL_Rect* clip, double angle, SDL_Point* center){
+	if (flip){
+		SDL_RenderCopyEx(renderer, mTexture, clip, &rect, angle, center, SDL_FLIP_HORIZONTAL);
+	}
+	else{
+		SDL_RenderCopyEx(renderer, mTexture, clip, &rect, angle, center, SDL_FLIP_NONE);
+	}
+}
+
+void GameObject::free()
+{
+    if (mTexture != NULL)
+    {
+        SDL_DestroyTexture(mTexture);
+        mTexture = NULL;
+        rect.w = 0;
+        rect.h = 0;
+    }
+}
---
+++ Run_Run_Run/GameObject.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Game.hpp"
+
+class GameObject
+{
+public:
+    GameObject();
+    ~GameObject();
+
+    void setRect(std::pair<int, int> pos) { rect.x = pos.first, rect.y = pos.second; }
+    void movePos(std::pair<int, int> pos);
+    SDL_Rect getRect() const { return rect; }
+    SDL_Texture* GetObject() const { return mTexture; }
+    std::pair<int, int> getPos() const; { return {rect.x, rect.y} }
+	std::pair<int, int> getSize() const; { return {rect.w, rect.h} }
+
+    virtual bool loadIMG(std::string path, SDL_Renderer* screen);
+    void render(SDL_Renderer* renderer, bool flip = 0,
+                 SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL);
+    void free();
+
+
+protected:
+    SDL_Texture* mTexture;
+    SDL_Rect rect;
+};
