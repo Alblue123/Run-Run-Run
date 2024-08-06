9bfe2f21a1fd2f9a983118c3c2bcd42ae04a7f4e
Alblue123 <newborniphone@gmail.com>
2024-08-07 02:24:11 +0700

    Adding class for handling animation

---
+++ Run_Run_Run/Animation.cpp
@@ -0,0 +1,62 @@
+#include "Animation.hpp"
+
+Animation::Animation(const std::string& name, std::pair<int, int> frameCount, Uint32 switchTime)
+    : frameCount(frameCount), switchTime(switchTime), currFrame(0, 0), currTime(0), flip(false) {
+    surface = IMG_Load(name.c_str());
+    if (loadedSurface == NULL) {
+        std::cout << "Cannot load image! SDL_image error: " << IMG_GetError() << std::endl;
+    }
+    box.w = surface->w / frameCount.first;
+    box.h = surface->h / frameCount.second;
+}
+
+Animation::~Animation() {
+    if (loadedSurface) {
+        SDL_FreeSurface(surface);
+    }
+}
+
+bool Animation::update(Uint32 deltaTime, std::pair<int, int> frameLimit, bool faceRight) {
+    bool check = false;
+    currTime += deltaTime;
+    if (currTime >= switchTime) {
+        currTime -= switchTime;
+        if (currFrame.second != frameLimit.second) {
+            currFrame = {0, frameLimit.second};
+        } else {
+            currFrame.first++;
+            if (currFrame.first >= frameLimit.first) {
+                currFrame.first = 0;
+                check = true;
+            }
+        }
+    }
+    update(faceRight);
+    return check;
+}
+
+bool Animation::update(Uint32 deltaTime, bool faceRight) {
+    return update(deltaTime, {frameCount.first, 0}, faceRight);
+}
+
+bool Animation::update(Uint32 deltaTime, std::pair<int, int> frameLimit) {
+    return update(deltaTime, frameLimit, flip);
+}
+
+bool Animation::update(Uint32 deltaTime) {
+    return update(deltaTime, flip);
+}
+
+void Animation::update(bool faceRight) {
+    box.y = currFrame.second * box.h;
+    box.x = currFrame.first * box.w;
+    flip = faceRight;
+}
+
+void Animation::update() {
+    update(flip);
+}
+
+void Animation::setSwitchTime(Uint32 switchTime) {
+    this->switchTime = switchTime;
+}
---
+++ Run_Run_Run/Animation.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <SDL.h>
+
+#include "Game.hpp"
+
+class Animation
+{
+public:
+    Animation(const std::string& name, std::pair<int, int> frameCount, Uint32 switchTime);
+    ~Animation();
+
+    bool update(Uint32 deltaTime, std::pair<int, int> frameLimit, bool faceRight);
+    bool update(Uint32 deltaTime, bool faceRight);
+    bool update(Uint32 deltaTime, std::pair<int, int> frameLimit);
+    bool update(Uint32 deltaTime);
+    void update(bool faceRight);
+    void update();
+
+    Uint32 getSwitchTime() const { return switchTime; }
+    void setSwitchTime(Uint32 switchTime);
+
+    std::pair<int, int> getFrameCount() const { return frameCount; }
+    SDL_Rect* getRect() { return box; }
+    bool getFlip() const { return flip; }
+
+private:
+    std::pair<int, int> frameCount;
+    Uint32 switchTime;
+    SDL_Surface* surface;
+    SDL_Rect box;
+    bool flip;
+
+    std::pair<int, int> currFrame;
+    Uint32 currTime;
+};
