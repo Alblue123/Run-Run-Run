9a58cbb42125e9f0eef528c253437f5b56520c15
Alblue123 <newborniphone@gmail.com>
2024-08-16 00:05:50 +0700

    first version of game

--- Run_Run_Run/Game.cpp
+++ Run_Run_Run/Game.cpp
@@ -5,18 +5,30 @@
 GameWindow* gWin = new GameWindow();
 SDL_Window* window;
 SDL_Renderer* renderer;
+Menu* menu;
+Instruction* instruction;
 
 extern const int FPS;
 extern const int frameDelay;
 Uint32 frameStart;
 unsigned int frameTime;
 
+int totalCherry = 0;
+int _totalHearts = 0;
+
+TTF_Font* font;
+
+
 bool HandleEvents(bool& quitGame);
 void renderGame();
 int play();
+int main();
+int instruct();
+int exit();
+void clean();
 
 
-enum Menu{
+enum MAINSCREEN {
 	PLAYGAME,
 	INSTRUCTION,
 	EXIT,
@@ -25,9 +37,30 @@ enum Menu{
 
 void start() {
     if(gWin->init()) {
-        int type = PLAYGAME;
-        play();
+        int type = MENU;
+        while (type != EXIT){
+            gWin = new GameWindow();
+            switch (type){
+                case MENU:
+                    type = main();
+                    break;
+                case INSTRUCTION:
+                    type = instruct();
+                    break;
+                case EXIT:
+                    type = exit();
+                    break;
+                case PLAYGAME:
+                    type = play();
+                    break;
+                default:
+                    break;
+            }
+            gWin->free();
+            delete gWin;
+            }
     }
+    clean();
 }
 
 bool HandleEvents(bool& quitGame) {
@@ -72,6 +105,7 @@ void renderGame() {
 
 int play() {
     bool quit = false, quitGame = false;
+    int currState = -1;
     Uint32 start_time = 0, current_time, deltaTime;
     gWin->setUp();
 
@@ -87,6 +121,7 @@ int play() {
             quit = true;
             break;
         }
+        if (currState != 1) gWin->update(deltaTime);
 
         renderGame();
 
@@ -101,4 +136,94 @@ int play() {
     }
 
    return -1;
+}
+
+int main() {
+    menu = new Menu();
+    menu->init();
+
+    Uint32 start_time = 0, current_time, deltaTime;
+	bool quit = false;
+    int state = -1;
+
+	while (!quit){
+		SDL_Event event;
+		frameStart = SDL_GetTicks();
+		current_time = SDL_GetTicks();
+		deltaTime = current_time - start_time;
+		start_time = current_time;
+		state = menu->update(deltaTime);
+		SDL_RenderClear(renderer);
+		while (SDL_PollEvent(&event) != 0){
+			if (event.type == SDL_QUIT){
+				quit = true;
+				return EXIT;
+			}
+		}
+        switch (state){
+            case -1:
+                menu->render();
+                break;
+            case INSTRUCTION:
+                return INSTRUCTION;
+            case EXIT:
+                return EXIT;
+            case PLAYGAME:
+                return PLAYGAME;
+            default:
+                break;
+        }
+
+		SDL_RenderPresent(renderer);
+		frameTime = SDL_GetTicks() - frameStart;
+		if (frameDelay > frameTime)
+		SDL_Delay(frameDelay - frameTime);
+    }
+    return -1;
+}
+
+int exit() {
+    return EXIT;
+}
+
+int instruct() {
+    instruction = new Instruction();
+    instruction->init();
+
+	Uint32 start_time = 0, current_time, deltaTime;
+	bool quit = false;
+
+    while (!quit){
+		SDL_Event event;
+		frameStart = SDL_GetTicks();
+		current_time = SDL_GetTicks();
+		deltaTime = current_time - start_time;
+		start_time = current_time;
+		SDL_RenderClear(renderer);
+		while (SDL_PollEvent(&event) != 0){
+			if (event.type == SDL_QUIT){
+				quit = true;
+				return EXIT;
+			}
+			else{
+				 if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_RETURN) return MENU;
+			}
+		}
+		instruction->render();
+		SDL_RenderPresent(renderer);
+		frameTime = SDL_GetTicks() - frameStart;
+		if (frameDelay > frameTime)
+		SDL_Delay(frameDelay - frameTime);
+    }
+    return -1;
+}
+
+void clean() {
+    SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	window = nullptr;
+	renderer = nullptr;
+	IMG_Quit();
+	TTF_Quit();
+	SDL_Quit();
 }
